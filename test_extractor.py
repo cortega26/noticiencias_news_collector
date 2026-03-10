962e19933a9ffe190bd8f8059acbd1465220b04d
Carlos Ortega <carlosortega77@gmail.com>
2026-03-10 20:00:56 -0300

    Image fix

---
+++ test_extractor.py
@@ -0,0 +1,9 @@
+import requests
+from news_collector.logic.parsers.image_extractor import ImageExtractor
+
+url = "https://scitechdaily.com/a-massive-star-suddenly-vanished-and-left-a-black-hole-behind/"
+html = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}).text
+extractor = ImageExtractor()
+cands = extractor.extract_candidates(html, url)
+for c in cands:
+    print(c)
